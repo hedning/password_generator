f178a9d8dd3f119f75cecac1fe4a5216b2fae33d
Tor Hedin Brønner <torhedinbronner@gmail.com>
2012-07-24 18:33:43 +0200

    A working password generator.
    Prints out to stdout at the moment which isn't ideal.

---
+++ pypass.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+# site:salt:length
+
+from argparse import ArgumentParser
+import bcrypt
+import os
+import getpass
+
+home = os.getenv('HOME')
+saltfile = home + '/.saltinfo'
+log_rounds = 25
+
+parser = ArgumentParser(description='Generates new passwords using a master password and an url.')
+
+parser.add_argument('site', type=str, nargs=1, help='The site you want to the password for')
+parser.add_argument('--generate', '-g', dest='generate', action='store_true',
+		help='will generate a new password, ie. a new salt')
+parser.add_argument('--length', '-l', dest='length', type=int, nargs=1, default=-1, help='the length of the hash')
+parser.add_argument('--store', '-s', dest='store', action='store_true',
+		help='will store the hash, so next time you call prog with site it will not prompt for the master password')
+parser.add_argument('--clean', dest='clean', action='store_true',
+		help='Cleans out hashes not in use')
+parser.add_argument('--undo', '-u', dest='undo', action='store_true',
+		help='Restores the recently used hash for the site.')
+
+options = parser.parse_args()
+site = options.site[0]
+
+
+def get_salt():
+	salts = [x for x in open(saltfile) if x[0] != '#']
+	index, info = 0, []
+	for index, v in enumerate(salts):
+		v = v.split(':')
+		if v[0] == site:
+			info = v
+			break
+	return salts, index, info
+	
+def write_salts():
+	fi = open(saltfile, 'w')
+	fi.write('\n'.join(salts))
+	fi.close()
+
+def generate_salt(length):
+	return ':'.join([site, bcrypt.gensalt(), str(length)])
+
+
+length = -1
+
+
+if options.generate:
+	salts, index, info = get_salt()
+	print info
+	if info != []:
+		length = options.length if options.length > 0 else int(info[2])
+		salt = generate_salt(length)
+		print 'Already exists a salt for this site. Generating new one, backing up the old'
+		salts[index] = salt
+		salts.insert(0, '#' + ':'.join(info))
+	else:
+		length = options.length
+		salt = generate_salt(length)
+		salts.append(salt)
+	write_salts()
+
+password = getpass.getpass()
+
+salts, index, info = get_salt()
+
+print bcrypt.hashpw(password+site, info[1]).replace(info[1], '')[:length]
+
